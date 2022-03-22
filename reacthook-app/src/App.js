78cc834607351530db2ee14102d2b4688bad0ae1
MasGenn <gentafatuh@gmail.com>
2022-03-22 17:36:46 +0700

    update file

--- reacthook-app/src/App.js
+++ reacthook-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import FormTodoList from "./component/FormTodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([
+    {
+      text: "Mengerjakan Exercise",
+      completed: false,
+    },
+    {
+      text: "Mengerjakan Assignment",
+      completed: true,
+    },
+  ]);
+
+  return <FormTodoList todos={todos} setTodos={setTodos} />;
 }
 
 export default App;
